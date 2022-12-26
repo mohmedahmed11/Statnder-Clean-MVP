a95f0284c9e6040d8050210da508a684b0a99d07
mohmedahmed11 <albrer112@gmail.com>
2022-12-26 23:33:41 +0200

    Initial Commit

---
+++ Statnder-Clean-MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Statnder-Clean-MVP
+//
+//  Created by Mohamed Ahmed on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
