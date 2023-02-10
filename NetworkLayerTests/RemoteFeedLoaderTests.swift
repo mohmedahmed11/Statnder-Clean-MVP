a55aff771ff2f154a4429dc31ca91853c73fca05
mohmedahmed11 <albrer112@gmail.com>
2023-02-10 22:01:12 +0200

    add more test assertions for deferent HTTP Responses

--- NetworkLayerTests/RemoteFeedLoaderTests.swift
+++ NetworkLayerTests/RemoteFeedLoaderTests.swift
@@ -51,12 +51,16 @@ final class Remote_Feed_Loader_Tests: XCTestCase {
     func test_lead_deliversErrorOnNon200HTTPResponce() {
         let (sut, client) = makeSUT()
 
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load {capturedErrors.append($0)}
         
-        client.complete(statusCode: 400)
         
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load {capturedErrors.append($0)}
+            client.complete(statusCode: code, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        } 
     }
     
     //MARK: - hellpers
