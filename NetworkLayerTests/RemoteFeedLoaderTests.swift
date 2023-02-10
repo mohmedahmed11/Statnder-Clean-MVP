9931e94f413337e7c9e13ff5dbae26661385f0ea
mohmedahmed11 <albrer112@gmail.com>
2023-02-10 14:16:55 +0200

    request data from url on load

--- NetworkLayerTests/RemoteFeedLoaderTests.swift
+++ NetworkLayerTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,35 @@
 import XCTest
  
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClinet.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClinet {
-    var requestedURL: String?
+    static let shared = HTTPClinet()
+    
+    var requestedURL: URL?
+    
+    private init() {}
 }
 
 final class Remote_Feed_Loader_Tests: XCTestCase {
 
     func test_init_doseNotRequestDataFromURL() {
-        let clinet = HTTPClinet()
+        let clinet = HTTPClinet.shared
         let _ = RemoteFeedLoader()
         
         XCTAssertNil(clinet.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let clinet = HTTPClinet.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(clinet.requestedURL)
     }
     
 }
