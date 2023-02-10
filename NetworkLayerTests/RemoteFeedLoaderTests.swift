dfb18aa5809805347693687f805ee1e6a3691615
mohmedahmed11 <albrer112@gmail.com>
2023-02-10 20:30:30 +0200

    combine spy Carter params in one argument

--- NetworkLayerTests/RemoteFeedLoaderTests.swift
+++ NetworkLayerTests/RemoteFeedLoaderTests.swift
@@ -57,16 +57,18 @@ final class Remote_Feed_Loader_Tests: XCTestCase {
     }
     
     class HTTPClinetSpy: HTTPClinet {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        var messages = [(url: URL, completion:(Error) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
         
         func get(with url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 
