5c1905ac45e887aaffece3c9ca70dab70f1a1817
mohmedahmed11 <albrer112@gmail.com>
2023-02-10 11:10:09 +0200

    create the feedLoader protocol boundry

---
+++ NetworkLayer/NetworkLayer.docc/Resources/Feeds/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  
+//
+//  Created by Mohamed Ahmed on 2/10/23.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
