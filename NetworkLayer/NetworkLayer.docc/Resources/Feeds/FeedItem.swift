07c119abf49c6b667d0923e91759e43d81776dec
mohmedahmed11 <albrer112@gmail.com>
2023-02-10 11:06:19 +0200

    create network layer framework add feed item model

---
+++ NetworkLayer/NetworkLayer.docc/Resources/Feeds/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  
+//
+//  Created by Mohamed Ahmed on 2/10/23.
+//
+
+import Foundation
+
+public class FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
