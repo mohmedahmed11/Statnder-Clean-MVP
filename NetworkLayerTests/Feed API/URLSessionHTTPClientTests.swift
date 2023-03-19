c01ad234b10e38150515971e8a42163f675e2002
mohmedahmed11 <albrer112@gmail.com>
2023-03-19 15:42:12 +0200

    move url creation to shared factory method

--- NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
+++ NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
@@ -37,7 +37,7 @@ class URLSessionHTTPClient {
      }
      
      func test_getFromURL_performGETRequestWithURL() {
-         let url = URL(string: "https://any-url.com")!
+         let url = anyURL()
          let exp = expectation(description: "waite for completion")
          
          URLProtocolStub.observeRequests { request in
@@ -52,13 +52,12 @@ class URLSessionHTTPClient {
      }
 
      func test_getFromURL_failsOnRequestError() {
-         let url = URL(string: "https://any-url.com")!
          let error = NSError(domain: "any error", code: 1)
          URLProtocolStub.stub(data: nil, response: nil, error: error)
          
          let exp = expectation(description: "waite for completion")
          
-         makeSUT().get(from: url) { result in
+         makeSUT().get(from: anyURL()) { result in
              switch result {
              case let .failuer(recivedError as NSError):
                  XCTAssertNotNil(recivedError)
@@ -78,6 +77,10 @@ class URLSessionHTTPClient {
          let sut = URLSessionHTTPClient()
          trackForMemoryLeaks(sut, file: file, line: line)
          return sut
+     }
+     
+     private func anyURL() -> URL {
+         return URL(string: "https://any-url.com")!
      }
      
      private class URLProtocolStub: URLProtocol {
