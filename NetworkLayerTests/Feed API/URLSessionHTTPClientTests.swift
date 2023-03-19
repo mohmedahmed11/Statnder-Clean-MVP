62594ab20076ee5642b04120ee093ae3d13a2a08
mohmedahmed11 <albrer112@gmail.com>
2023-03-19 15:33:22 +0200

    move SUT (system under test ) creation method to helper method for avoid test braking changes

--- NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
+++ NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
@@ -46,7 +46,7 @@ class URLSessionHTTPClient {
              exp.fulfill()
          }
          
-         URLSessionHTTPClient().get(from: url) { _ in }
+         makeSUT().get(from: url) { _ in }
          
          wait(for: [exp], timeout: 1.0)
      }
@@ -56,11 +56,9 @@ class URLSessionHTTPClient {
          let error = NSError(domain: "any error", code: 1)
          URLProtocolStub.stub(data: nil, response: nil, error: error)
          
-         let sut = URLSessionHTTPClient()
-         
          let exp = expectation(description: "waite for completion")
          
-         sut.get(from: url) { result in
+         makeSUT().get(from: url) { result in
              switch result {
              case let .failuer(recivedError as NSError):
                  XCTAssertNotNil(recivedError)
@@ -75,6 +73,10 @@ class URLSessionHTTPClient {
      }
 
      // MARK: - Helpers
+     
+     private func makeSUT() -> URLSessionHTTPClient{
+         return URLSessionHTTPClient()
+     }
      
      private class URLProtocolStub: URLProtocol {
          
