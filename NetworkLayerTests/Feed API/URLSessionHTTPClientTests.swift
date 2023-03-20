16fb7fc0be282ee0076997536c9e69dd407f0e99
mohmedahmed11 <albrer112@gmail.com>
2023-03-20 13:06:43 +0200

    deliver data and Response on successful HTTP URL Request

--- NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
+++ NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,10 +18,12 @@ class URLSessionHTTPClient {
      }
 
     func get(from url: URL, compeletion: @escaping  (HTTPClientResult) -> Void) {
-         session.dataTask(with: url) { _, _, error in
+         session.dataTask(with: url) { data, response, error in
              if let error = error {
                  compeletion(.failuer(error))
-             }else {
+             }else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+                 compeletion(.success(response, data))
+             } else {
                  compeletion(.failuer(UnexpectedValuesRepresentation()))
              }
          }.resume()
@@ -74,6 +76,28 @@ class URLSessionHTTPClient {
          XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: anyNSError()))
          XCTAssertNotNil(resultErrorFor(data: anyData(), response: anyHTTPURLResponse(), error: anyNSError()))
          XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil))
+     }
+     
+     func test_getFromURL_suceedsOnHTTPURLResponseWithData() {
+         let data = anyData()
+         let response = anyHTTPURLResponse()
+         URLProtocolStub.stub(data: data, response: response)
+         
+         let exp = expectation(description: "waite for completion")
+         
+         makeSUT().get(from: anyURL()) { result in
+             switch result {
+             case let .success(recivedRespnse, recivedData):
+                 XCTAssertEqual(recivedData, data)
+                 XCTAssertEqual(recivedRespnse.url, response.url)
+                 XCTAssertEqual(recivedRespnse.statusCode, response.statusCode)
+             default:
+                 XCTFail("expected seccess but we go \(result) instade")
+             }
+             exp.fulfill()
+         }
+         
+         wait(for: [exp], timeout: 1.0)
      }
 
      // MARK: - Helpers
