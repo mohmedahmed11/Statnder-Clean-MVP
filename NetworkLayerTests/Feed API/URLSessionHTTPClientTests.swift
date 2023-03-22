9cf5174ee2489f3f8b8c1a287e67904c0569c04c
mohmedahmed11 <albrer112@gmail.com>
2023-03-22 10:57:39 +0200

    move URLSessionHTTPClient to its own file in production

--- NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
+++ NetworkLayerTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,28 +8,6 @@
 import XCTest
 import NetworkLayer
 
-class URLSessionHTTPClient: HTTPClinet {
-     private let session: URLSession
-    
-    struct UnexpectedValuesRepresentation: Error {}
-
-    init(session: URLSession = .shared) {
-         self.session = session
-     }
-
-    func get(from url: URL, compeletion: @escaping  (HTTPClientResult) -> Void) {
-         session.dataTask(with: url) { data, response, error in
-             if let error = error {
-                 compeletion(.failuer(error))
-             }else if let data = data, let response = response as? HTTPURLResponse {
-                 compeletion(.success(response, data))
-             } else {
-                 compeletion(.failuer(UnexpectedValuesRepresentation()))
-             }
-         }.resume()
-     }
- }
-
  class URLSessionHTTPClientTests: XCTestCase {
      
      override func setUp() {
